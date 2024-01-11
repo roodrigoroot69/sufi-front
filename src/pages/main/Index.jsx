91a22e2c8ae9c240efe1aa58d4871576c1af376d
Rodrigo <fraurcino@hotmail.com>
2024-01-11 10:15:20 -0600

    feat: add container cards

--- src/pages/main/Index.jsx
+++ src/pages/main/Index.jsx
@@ -2,15 +2,16 @@ import Container from "react-bootstrap/esm/Container";
 import CategoriesNav from "../../components/navbars/CategoriesNav";
 import NavbarGeneral from "../../components/navbars/NavbarGeneral";
 import SearchForm from "../../components/forms/SearchForm";
-
+import Cards from "../../components/cards/CardContainer";
 
 const Index = () => {
   return (
     <div>
       <NavbarGeneral/>
-      <Container>
+      <Container fluid>
         <CategoriesNav/>
         <SearchForm/>
+        <Cards/>
       </Container>
     </div>
   );
